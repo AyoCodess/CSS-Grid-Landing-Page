347806414e724b92aa639bfab75f74d9ca3eee3f
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-02-26 15:41:18 +0100

    header v1

--- dist/main.07544d9b.js
+++ dist/main.07544d9b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/back.jpg":[["back.9cd3ab62.jpg","img/back.jpg"],"img/back.jpg"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/hero.jpeg":[["hero.ac0612d9.jpeg","img/hero.jpeg"],"img/hero.jpeg"],"./../img/back.jpg":[["back.9cd3ab62.jpg","img/back.jpg"],"img/back.jpg"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
